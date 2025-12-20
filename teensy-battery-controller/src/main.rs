647a6173a7748bcf3f7fd709623aca743ec7a364
Sebastian Kuzminsky <seb@highlab.com>
2025-12-20 09:59:35 -0700

    teensy-battery-controller: read CAN packets from battery

--- teensy-battery-controller/src/main.rs
+++ teensy-battery-controller/src/main.rs
@@ -1,12 +1,5 @@
 //! The starter code slowly blinks the LED and sets up
 //! USB logging. It periodically logs messages over USB.
-//!
-//! Despite targeting the Teensy 4.0, this starter code
-//! should also work on the Teensy 4.1 and Teensy MicroMod.
-//! You should eventually target your board! See inline notes.
-//!
-//! This template uses [RTIC v2](https://rtic.rs/2/book/en/)
-//! for structuring the application.
 
 #![no_std]
 #![no_main]
@@ -37,6 +30,8 @@ mod app {
         led: board::Led,
         /// A poller to control USB logging.
         poller: logging::Poller,
+        /// The CAN1 interface on pins 22 (Tx) and 23 (Rx)
+        can1: board::Flexcan1,
     }
 
     #[init]
@@ -45,11 +40,17 @@ mod app {
             mut gpio2,
             pins,
             usb,
+            flexcan1,
             ..
         } = my_board(cx.device);
 
         let led = board::led(&mut gpio2, pins.p13);
         let poller = logging::log::usbd(usb, logging::Interrupts::Enabled).unwrap();
+
+        let mut can1 = board::flexcan(flexcan1, pins.p22, pins.p23);
+        can1.set_baud_rate(500_000);
+        can1.set_max_mailbox(16);
+        can1.disable_fifo();
 
         Systick::start(
             cx.core.SYST,
@@ -58,7 +59,9 @@ mod app {
         );
 
         blink::spawn().unwrap();
-        (Shared {}, Local { led, poller })
+        battery_task::spawn().unwrap();
+
+        (Shared {}, Local { led, poller, can1 })
     }
 
     #[task(local = [led])]
@@ -77,6 +80,30 @@ mod app {
             }
 
             count = count.wrapping_add(1);
+        }
+    }
+
+    #[task(local = [can1])]
+    async fn battery_task(context: battery_task::Context) {
+        let can = context.local.can1;
+
+        loop {
+            // read all available mailboxes for any available frames
+            if let Some(data) = can.read_mailboxes() {
+                // TODO: get rid of this and make it a "real" message
+                let frame_id = data.frame.id();
+                let id = match frame_id {
+                    imxrt_hal::can::Id::Standard(v) => v.as_raw().into(),
+                    imxrt_hal::can::Id::Extended(v) => v.as_raw(),
+                };
+
+                // Decode the CAN message
+                if let Some(payload) = data.frame.data() {
+                    log::info!("{:?} {:?}", id, payload);
+                }
+            }
+
+            Systick::delay(5.millis()).await;
         }
     }
 
