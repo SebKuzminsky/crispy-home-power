983f15bbbf440c604e8744ba5ffedc42f52cd10c
Sebastian Kuzminsky <seb@highlab.com>
2025-12-23 10:36:45 -0700

    teensy: rename the firware to "teensy-battery-controller"
    Not "hello-world" lol

--- teensy-battery-controller/build.sh
+++ teensy-battery-controller/build.sh
@@ -2,6 +2,6 @@
 set -e
 
 cargo build --offline --release
-cargo objcopy --release -- -O ihex hello-world.hex
+cargo objcopy --release -- -O ihex teensy-battery-controller.hex
 echo "click Reset on teensy"
-teensy_loader_cli --mcu=TEENSY40 -w hello-world.hex
+teensy_loader_cli --mcu=TEENSY40 -w teensy-battery-controller.hex
